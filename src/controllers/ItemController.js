cafd8aa81b69cc36ea74407b20e73f17d270832c
Carlos <carlosifsm@gmail.com>
2023-11-03 14:52:17 -0300

    final

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -1,23 +1,46 @@
 const knex = require('../database')
 
 module.exports = {
-    async index(req, res) { 
-        const results = await knex('item')
+    async index(req, res, next) { 
+        try{
+            if(!!req.params.id){
+                const { id } = req.params
+                const result = await knex('item')
+                                    .where({ id })
+                                    .first()
 
-        return res.json(results)
+                if(!result)throw new Error("Item não existe.");
+
+                return res.json(result)
+
+            }else{
+                const results = await knex('item')
+                return res.send(results)
+            }
+        }catch (error) {
+            res.send({message:error.message})
+            next(error)
+        }
     },
     async create(req, res, next) {
         try {
             const { nome, qtd } = req.body
+            const fields = ['nome', 'qtd'];
+            const valid = fields.reduce((acc, f) => (acc && !!req.body[f]), true);
+
+            if(!valid) throw new Error('Solicitação inválida.')
 
             await knex('item').insert({
                 nome,
                 qtd
             })
 
-            return res.status(201).send()
+            return res.status(201).send({message:"Item Criado."})
         } catch (error) {
-            next(error)
+            res.send({
+                message:error.message
+              })
+            //next(error)
         }
     },
     async update(req, res, next) {
@@ -26,29 +49,39 @@ module.exports = {
             const { qtd } = req.body
             const { id } = req.params
             
+            const item = await knex('item')
+                                .where({id})
+                                .first()
             
+            if(!item)throw new Error("Item não existe.");
+
             await knex('item')
             .update({ nome })
             .update({ qtd })
             .where({ id })
-
-            return res.send()
+            
+            return res.send({message:"Item Atualizado."})
 
         } catch (error) {
+            res.send({message:error.message})
             next(error)
         }
     },
     async delete(req, res, next) {
         try {
             const { id } = req.params
+            const item = await knex('item')
+                                .where({id})
+                                .first()
+            if(!item)throw new Error("Item não existe");
 
             await knex('item')
-            .where({ id })
-            .del()
-            
+                .where({ id })
+                .del()
 
-            return res.send()
+            return res.send({message:"Item Deletado"})
         } catch (error) {
+            res.send({message:error.message})
             next(error)
         }
     }
--- src/routers/item.js
+++ src/routers/item.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 const ItemController = require('../controllers/ItemController')
 
 routes
-    .get('/item', ItemController.index)
+    .get('/item/:id?', ItemController.index)
     .post('/item', ItemController.create)
     .put('/item/:id', ItemController.update)
     .delete('/item/:id', ItemController.delete)
